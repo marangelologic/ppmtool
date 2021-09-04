0bd637eae4972804ac3ea4ff022089dca9624f3b
nullLogic2408 <mar.angelo.logic@gmail.com>
2021-09-04 19:57:12 +0800

    fixed the error message

--- src/main/java/com/example/react/controller/ProjectController.java
+++ src/main/java/com/example/react/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package com.example.react.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -11,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/project")
-@Validated
 public class ProjectController {
 
     @Autowired
@@ -35,7 +35,10 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
         
         if(result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid part of the object", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMessageMap = new HashMap<>();
+            result.getFieldErrors().forEach(error-> errorMessageMap.put(error.getField(), error.getDefaultMessage()));
+
+            return new ResponseEntity<Map<String,String>>(errorMessageMap, HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<Project>(service.createNewProject(project), HttpStatus.CREATED);
     }
